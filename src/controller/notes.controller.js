c2423a1a622e0f671af634384e525bab07d2c759
Aakash S <aakash@Aakashs-MacBook-Air.local>
2024-04-09 22:58:09 +0530

    Created Controller,router,model,databse

--- src/controller/notes.controller.js
+++ src/controller/notes.controller.js
@@ -11,10 +11,23 @@ const getNotes = async(req,res)=>{
     }
 };
 
+const getNote = async(req,res)=>{
+    try{
+        const {id} = req.params;
+        const data = await Notes.findById(id);
+        if(!data) throw new err("No notes found");
+        res.status(200).json(data);
+    }
+    catch(err){
+        res.status(500).json({msg:"Error fetching the data"});
+    }
+};
+
 const createNotes = async (req, res) => {
   try {
     const { title, description } = req.body;
     const newNote = await Notes.create({ title, description });
+    if(!newNote) throw new err("Error creating new notes")
     res.status(201).json(newNote);
   } catch (err) {
     console.error(err);
@@ -22,5 +35,29 @@ const createNotes = async (req, res) => {
   }
 };
 
+const updateNote = async(req,res)=>{
+    try{
+        const {title,description} = req.body;
+        const {id} = req.params;
+        const updatedNote = await Notes.findByIdAndUpdate(id,{title,description});
+        if(!updatedNote) throw new err("Unable to find the Note");
+        res.status(201).json(updatedNote);
+    }catch(err){
+        res.status(500).json({msg:"Unable to update the Notes"});
+    }
+}
 
-module.exports = {createNotes , getNotes};
+const deleteNote = async(req,res)=>{
+    try{
+        const {id} = req.params;
+        const data = await Notes.findByIdAndDelete(id);
+        if(!data) throw new err("Error deleting the notes");
+        res.status(200).json(data);
+    }
+    catch{
+        res.status(500).json({msg:"Error deleting the Note"});
+    }
+}
+
+
+module.exports = {createNotes , getNotes , getNote , updateNote , deleteNote};
--- src/router/notes.router.js
+++ src/router/notes.router.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { createNotes , getNotes } = require("../controller/notes.controller");
+const { createNotes , getNotes , getNote , updateNote, deleteNote} = require("../controller/notes.controller");
 
 router.get("/", getNotes); // Route for 
+router.get("/:id", getNote); // Route for 
 router.post("/", createNotes); // Route for creating notes
+router.put("/:id",updateNote)
+router.delete("/:id",deleteNote);
 
 module.exports = router;
