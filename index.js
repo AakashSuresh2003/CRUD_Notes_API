c2ec0247a4404165ded56e69546ae7b52ceca933
Aakash S <aakash@Aakashs-MacBook-Air.local>
2024-04-22 23:51:21 +0530

    Feat : Created Routes for Authentication

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const ConnectDB = require("./src/DataBase/notesDB");
 const Notes = require("./src/models/notes.models");
 const notesRoute = require("./src/router/notes.router");
+const authRouter = require("./src/router/auth.router");
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+
 ConnectDB();
 
 app.get("/", (req, res) => {
@@ -14,6 +18,8 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/v1/notes", notesRoute);
+app.use("/api/user",authRouter);
+
 
 app.listen(3000, (req, res) => {
   console.log("Server listening on Port 3000");
